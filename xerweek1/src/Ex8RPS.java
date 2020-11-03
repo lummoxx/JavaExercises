78c0e3f8c72abc0d4083f0fabbde23a5ae0cd645
sluggerbit <70601563+sluggerbit@users.noreply.github.com>
2020-11-03 17:33:41 +0100

    test push :hand:
    snälla funkaa

--- xerweek1/src/Ex8RPS.java
+++ xerweek1/src/Ex8RPS.java
@@ -24,6 +24,7 @@ public class Ex8RPS {
         new Ex8RPS().program();
     }
 
+
     final Random rand = new Random();
     final Scanner sc = new Scanner(in);
 
@@ -35,20 +36,49 @@ public class Ex8RPS {
         int result;         // Result for this round
         int round = 0;      // Number of rounds
         int total = 0;      // Final result after all rounds
+        int rmin = 1;
+        int rmax = 3;
 
         // All code here ... (no method calls)
+        // BRB, 10 min******************
 
         out.println("Welcome to Rock, Paper and Scissors");
-
+        //Rock (1) <-- Paper (2) <-- Scissors (3)
+        result = 0;
         while (round < maxRounds) {   // Game loop
-            // -------- Input --------------
-
-
-            // ----- Process -----------------
-
-
-            // ---------- Output --------------
-
+            out.println("Select 1, 2 or 3 (for R, P or S) >");
+            int choice = sc.nextInt();
+            int comp = rand.nextInt((rmax - rmin) + 1) + rmin;
+            switch (choice) {
+                case 1:
+                    if (comp == 2) {
+                        result -= 1;
+                        out.println("Computer won");
+                    } else if (comp == 3) {
+                        result += 1;
+                        out.println("You won");
+                    } else out.println("A draw");
+                    break;
+                case 2:
+                    if (comp == 3) {
+                        result -= 1;
+                        out.println("Computer won");
+                    } else if (comp == 1) {
+                        result += 1;
+                        out.println("You won");
+                    } else out.println("A draw");
+                    break;
+                case 3:
+                    if (comp == 1) {
+                        result -= 1;
+                        out.println("Computer won");
+                    } else if (comp == 2) {
+                        result += 1;
+                        out.println("You won");
+                    } else out.println("A draw");
+                    break;
+            }
+            out.println("Result: " + result);
 
             round++;
         }
@@ -63,3 +93,41 @@ public class Ex8RPS {
         }
     }
 }
+
+/*
+8.  See Ex8RPS. Implement a program for the game Rock, Paper and Scissor.
+    NOTE: The main task here is to use smallest step programming.
+    You don't need any methods for this, put all code in program().
+
+    The game should run as (just 5 rounds):
+
+    Welcome to Rock, Paper and Scissors
+    Select 1, 2 or 3 (for R, P or S) > 1
+    Computer choose: 2
+    You won
+    Result 1                                (use an int for over all result, +1 for human, total = 1)
+
+    Select 1, 2 or 3 (for R, P or S) > 2
+    Computer choose: 2
+    A draw
+    Result 1
+
+    Select 1, 2 or 3 (for R, P or S) > 3
+    Computer choose: 1
+    You won
+    Result 2                                (result, +1 for human, total = 2)
+
+    Select 1, 2 or 3 (for R, P or S) > 1
+    Computer choose: 1
+    A draw
+    Result 2
+
+    Select 1, 2 or 3 (for R, P or S) > 2
+    Computer choose: 1
+    Computer won
+    Result 1                        (total = 1 -> i.e. total positive -> human won)
+
+    Game over!
+    Human won.
+
+*/
