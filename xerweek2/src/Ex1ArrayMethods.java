a1f39ebacdfc17865358e6076f806141f9f55840
sluggerbit <70601563+sluggerbit@users.noreply.github.com>
2020-11-06 17:38:22 +0100

    Week2 ex1+2 :wind_chime:
    did ex1
    started x2 nestled arrays

--- xerweek2/src/Ex1ArrayMethods.java
+++ xerweek2/src/Ex1ArrayMethods.java
@@ -23,34 +23,109 @@ public class Ex1ArrayMethods {
     final static Random rand = new Random();
 
     void program() {
-        int[] arr = {1, 2, 2, 5, 3, 2, 4, 2, 7};  // Hard coded test data
-
+        // test();
+        int[] arr = { 1, 2, 2, 5, 3, 2, 4, 2, 7 }; // Hard coded test data
+        int[] arr2 = new int[0];
+        int[] arr3 = new int[0];
         // Uncomment one at a time and implement
 
         // Count occurrences of some element in arr
-        //out.println(count(arr, 2) == 4);      // There are four 2's
-        //out.println(count(arr, 7) == 1);
+        out.println(count(arr, 2) == 4); // There are four 2's
+        out.println(count(arr, 7) == 1); // There is one 7
 
-        // Generate array with 100 elements with 25% distribution of -1's and 1's (remaining will be 0)
-        //arr = generateDistribution(100, 0.25, 0.25);
-        //out.println(count(arr, 1) == 25);
-        //out.println(count(arr, -1) == 25);
-        //out.println(count(arr, 0) == 50);
-
+        // Generate array with 100 elements with 25% distribution of -1's and 1's
+        // (remaining will be 0)
+        arr2 = generateDistribution(100, 0.25, 0.25);
+        out.println(count(arr2, 1) == 25);
+        out.println(count(arr2, -1) == 25);
+        out.println(count(arr2, 0) == 50);
         // Generate array with 14 elements with 40% 1's and 30% -1's
-        //arr = generateDistribution(14, 0.4, 0.3);
-        //out.println(count(arr, 1) == 6);
-        //out.println(count(arr, -1) == 4);
+        arr3 = generateDistribution(14, 0.4, 0.3);
+        out.println(count(arr3, 1) == 6);
+        out.println(count(arr3, -1) == 4);
 
         for (int i = 0; i < 10; i++) {
             // Random reordering of arr, have to check by inspecting output
-            //shuffle(arr);
-            out.println(Arrays.toString(arr));  // Does it look random?
+            shuffle(arr3);
+            out.println(Arrays.toString(arr3)); // Does it look random?
         }
     }
-
-
     // ---- Write methods below this ------------
 
+    int count(int[] arr, int num) {
+        int occurences = 0;
+        int length = arr.length;
+
+        for (int i = 0; i < length; i++) {
+            if (arr[i] == num) {
+                occurences++;
+            }
+        }
+        return occurences;
+    }
+
+    // (-1,1)
+    int[] generateDistribution(int elements, double percentPos, double percentNeg) {
+        int[] arr = new int[elements];
+        out.println(Arrays.toString(arr));
+        double numPos = percentPos * elements;
+        double numNeg = percentNeg * elements;
+        int ettor = (int) Math.round(numPos);
+        int minusettor = (int) Math.round(numNeg);
+
+        for (int i = 0; i < ettor; i++) {
+            arr[i] = 1;
+        }
+        for (int i = ettor; i < minusettor + ettor; i++) {
+            arr[i] = -1;
+        }
+        return arr;
+
+    }
+
+    void test() {
+        generateDistribution(100, 0.25, 0.25);
+        exit(0);
+    }
+
+    int[] shuffle(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            int randomIndexToSwap = rand.nextInt(arr.length - 1); // en int mellan 0-99 = 81
+            int temp = arr[randomIndexToSwap]; // vdet som låg på index 81
+            arr[randomIndexToSwap] = arr[i]; // arr[81] får det som är på arr[0]
+            arr[i] = temp; // arr[i] = det som låg på plats 81
+        }
+        return arr;
+    }
 
 }
+/*
+ * See Ex2ArrayMethod. Implement methods. In general program should just print
+ * "true". The only exception is the shuffle method. Method uses Random, so hard
+ * to test. Print out for shuffle should look something like:
+ *
+ * [0, 0, 1, 0, -1, -1, 1, 0, 1, 1, 1, 1, -1, -1] (randomly ordered elements)
+ * [-1, 1, 1, 0, 0, 1, -1, 1, 1, 0, 1, -1, 0, -1] [-1, 1, 1, 0, 1, 0, -1, 0, 1,
+ * 1, -1, 1, 0, -1] [-1, -1, 0, 0, -1, -1, 1, 1, 0, 1, 1, 1, 1, 0] [0, 0, 1, -1,
+ * -1, 0, -1, -1, 1, 1, 1, 1, 1, 0] [1, -1, 0, 0, 1, -1, 1, 1, 0, 1, -1, 0, 1,
+ * -1] [1, 1, 1, -1, 0, 1, 0, 0, 1, -1, -1, 0, 1, -1] [-1, 1, 1, 0, -1, 0, -1,
+ * 1, 1, 1, 0, 1, 0, -1] [1, 1, -1, -1, -1, 1, 0, 1, -1, 0, 1, 0, 0, 1] [0, 1,
+ * 0, 1, 0, 0, 1, 1, -1, -1, 1, 1, -1, -1] ...
+ *
+ * See code for comments.
+ * 
+ * 
+ * [0, 1, 0, 1, 1, 1, 1, -1, -1, -1, 0, -1, 1, 0] [0, 1, 1, 1, 1, 0, 0, -1, 1,
+ * 1, -1, 0, -1, -1] [1, 1, 0, -1, 1, -1, -1, 1, 1, 0, -1, 0, 1, 0] [1, 0, -1,
+ * 1, -1, 0, 0, 1, -1, -1, 1, 0, 1, 1] [1, -1, -1, 0, -1, 1, 0, 1, 0, -1, 1, 0,
+ * 1, 1] [0, 1, -1, 0, 1, -1, 0, 0, -1, 1, 1, -1, 1, 1] [1, -1, 0, 1, 1, 1, -1,
+ * 1, 1, -1, 0, 0, 0, -1] [-1, 1, 0, -1, 1, 0, -1, 1, 0, -1, 1, 1, 1, 0] [-1,
+ * -1, 1, 1, 1, 1, 0, 1, -1, 0, 0, 0, 1, -1] [-1, 1, 0, 1, -1, 1, 0, 1, 1, 0, 1,
+ * -1, -1, 0]
+ * 
+ * Process finished with exit code 0
+ * 
+ * 
+ * 
+ */
--- xerweek2/src/Ex2MatrixMethods.java
+++ xerweek2/src/Ex2MatrixMethods.java
@@ -18,81 +18,115 @@ public class Ex2MatrixMethods {
     }
 
     void program() {
-        int[][] m = {           // Hard coded test data
-                {-1, 0, -5, 3},
-                {6, 7, -2, 0},
-                {9, -2, -6, 8},
-                {0, 0, 5, -6}
-        };
+
+        int[][] m = { // Hard coded test data
+                { -1, 0, -5, 3 }, // twodimensional array, array of 4 elements, each elements
+                { 6, 7, -2, 0 }, { 9, -2, -6, 8 }, { 0, 0, 5, -6 } };
+
+        // test(m);
 
         // Uncomment one at a time and implement
 
         // Return array with all negatives in m
-        /*int[] negs = getNegatives(m);
+        int[] negs = getNegatives(m);
         out.println(negs.length == 6);
-        out.println(Arrays.toString(negs).equals("[-1, -5, -2, -2, -6, -6]")); // Possibly other ordering!
-        */
+        out.println(Arrays.toString(negs).equals("[-1, -5, -2, -2, -6, -6]")); //
+        // Possibly other ordering!
+
         // Mark all negatives with a 1, others as 0
         // (create matrix on the fly)
-        /*
-        int[][] marked = markNegatives(new int[][]{
-                {1, -2, 3,},
-                {-4, 5, -6,},
-                {7, -8, 9,},
-        });
-        */
+        int[][] marked = markNegatives(new int[][] { { 1, -2, 3, }, { -4, 5, -6, }, { 7, -8, 9, }, });
 
-        /* marked should be
-        { {0, 1, 0},
-          {1, 0, 1},
-          {0, 1, 0} }
-        */
         /*
+         * marked should be { {0, 1, 0}, {1, 0, 1}, {0, 1, 0} }
+         */
+
         out.println(Arrays.toString(marked[0]).equals("[0, 1, 0]"));
         out.println(Arrays.toString(marked[1]).equals("[1, 0, 1]"));
         out.println(Arrays.toString(marked[2]).equals("[0, 1, 0]"));
-        */
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
         // Create matrix from array
 
-        //int[][] matrix = toMatrix(arr);
-        /* matrix should be
-        { {1, 2, 3},
-          {4, 5, 6},
-          {7, 8, 9} }
-        */
-        //plot(matrix);  // If manual inspection
+        // int[][] matrix = toMatrix(arr);
         /*
-        out.println(Arrays.toString(matrix[0]).equals("[1, 2, 3]"));
-        out.println(Arrays.toString(matrix[1]).equals("[4, 5, 6]"));
-        out.println(Arrays.toString(matrix[2]).equals("[7, 8, 9]"));
-        */
+         * matrix should be { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} }
+         */
+        // plot(matrix); // If manual inspection
+        /*
+         * out.println(Arrays.toString(matrix[0]).equals("[1, 2, 3]"));
+         * out.println(Arrays.toString(matrix[1]).equals("[4, 5, 6]"));
+         * out.println(Arrays.toString(matrix[2]).equals("[7, 8, 9]"));
+         */
         // Sum of all directly surrounding elements to some element in matrix
         // (not counting the element itself)
         // NOTE: Should be possible to expand method to include more distant neighbours
         /*
-        out.println(sumNeighbours(matrix, 0, 0) == 11);
-        out.println(sumNeighbours(matrix, 1, 1) == 40);
-        out.println(sumNeighbours(matrix, 1, 0) == 23);
-
+         * out.println(sumNeighbours(matrix, 0, 0) == 11);
+         * out.println(sumNeighbours(matrix, 1, 1) == 40);
+         * out.println(sumNeighbours(matrix, 1, 0) == 23);
+         * 
          */
     }
 
     // -------- Write methods below this -----------------------
 
-
     boolean isValidLocation(int size, int row, int col) {
         return row >= 0 && col >= 0 && row < size && col < size;
     }
 
-
-    // Use if you like (during development)
-    void plot(int[][] matrix) {
-        for (int row = 0; row < matrix.length; row++) {
-            out.println(Arrays.toString(matrix[row]));
+    int[] getNegatives(int[][] m) {
+        int[] neg = new int[m.length * m[0].length]; // [];
+        int neglength = 0;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < ((m[0].length)); j++) {
+                if (m[i][j] < 0) {
+                    neg[neglength] = m[i][j];
+                    neglength++;
+                }
+            }
         }
+        int[] neg2 = new int[neglength];
+        for (int i = 0; i < neglength; i++) {
+            neg2[i] = neg[i];
+        }
+        return neg2;
     }
 
+    int[][] markNegatives(int[][] m) {
+        int[][] marked = new int[m.length][m[0].length];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                if (m[i][j] > 0) {
+                    marked[i][j] = 0;                    
+                } else {
+                    marked[i][j] = 1;
+                }
+            }
+        }
+        return marked;
+    }
 
+    void test(int[][] m) {
+        out.println("utanför: " + m.length);
+        out.println("inuti: " + m[0].length);
+        out.println(Arrays.toString(getNegatives(m)));
+        exit(0);
+    }
+
+    private void exit(int i) {
+    }
 }
+
+/*
+ * 2. See Ex2MatrixMethods. Implement methods so that program prints true for
+ * everything. Use functional decomposition if things getting too complex.
+ * 
+ * [[I@7cc355be, [I@6e8cf4c6, [I@12edcd21, [I@34c45dca]
+ * 
+ * 
+ * 
+ * // Use if you like (during development) void plot(int[][] matrix) { for (int
+ * row = 0; row < matrix.length; row++) {
+ * out.println(Arrays.toString(matrix[row])); }
+ */
